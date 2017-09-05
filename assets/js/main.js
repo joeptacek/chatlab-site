f652a2843d64f4759002209cb10889b0539a19df
Joe Ptacek <joe.ptacek@gmail.com>
2017-09-04 23:31:47 -0400

    fill out index, add js for sticky nav-page, fiddle with margin-bottoms

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
-window.onload = function() {
+var at_top = true;
 
-};
+window.addEventListener("scroll", function() {
+  var scroll_Y = window.pageYOffset;
+
+  if (at_top) {
+    if (scroll_Y > 227) {
+      at_top = false;
+      // Only fires once when crossing from low to high
+      document.getElementById("nav-page").classList.add("scrolled");
+    }
+  } else {
+    if (scroll_Y < 228) {
+      at_top = true;
+      // Only fires once when crossing from high to low
+      document.getElementById("nav-page").classList.remove("scrolled");
+    }
+  }
+});
