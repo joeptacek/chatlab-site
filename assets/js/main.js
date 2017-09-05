3acd13a54bf0183ffd48d1ff79150cdd79b5fb1f
Joe Ptacek <joe.ptacek@gmail.com>
2017-09-05 17:33:39 -0400

    nav-site: improved styling, added js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
+document.getElementById("nav-site-main").classList.add("hidden");
+
 var at_top = true;
+var menu_visible = false;
 
 window.addEventListener("scroll", function() {
   var scroll_Y = window.pageYOffset;
@@ -15,5 +18,15 @@ window.addEventListener("scroll", function() {
       // Only fires once when crossing from high to low
       document.getElementById("nav-page").classList.remove("scrolled");
     }
+  }
+});
+
+document.getElementById("nav-site-toggle-button").addEventListener("click", function() {
+  if (menu_visible) {
+    menu_visible = false;
+    document.getElementById("nav-site-main").classList.add("hidden")
+  } else {
+    menu_visible = true;
+    document.getElementById("nav-site-main").classList.remove("hidden")
   }
 });
