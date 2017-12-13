6a141bce184386a98226bfc9069b5681321651f5
Joe Ptacek <joe.ptacek@gmail.com>
2017-12-13 13:02:34 -0500

    minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
-var child = require('child_process');
-// var sourcemaps = require('gulp-sourcemaps');
+var spawn = require('child_process').spawn;
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
@@ -13,23 +12,19 @@ gulp.task('css', function () {
       cssnano()
   ];
   return gulp.src('_assets/sass/**/*.scss')
-    // .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(plugins))
-    // .pipe(sourcemaps.write())
     .pipe(gulp.dest('_site/assets/css'));
 });
 
 gulp.task('js', function () {
   return gulp.src('_assets/js/**/*.js')
-    // .pipe(sourcemaps.init())
     .pipe(uglify())
-    // .pipe(sourcemaps.write())
     .pipe(gulp.dest('_site/assets/js'));
 });
 
 gulp.task('jekyll', function (done) {
-  return child.spawn('bundle', ['exec', 'jekyll', 'build', '--incremental'], {stdio: 'inherit'})
+  return spawn('bundle', ['exec', 'jekyll', 'build', '--incremental'], {stdio: 'inherit'})
     .on('close', done);
 });
 
