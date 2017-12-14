03072e70330d6e798a4b95ee22fb76e600b8f215
Joe Ptacek <joe.ptacek@gmail.com>
2017-12-14 17:44:35 -0500

    added gulp-util and gulp-debug, closing in on final jekyll task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
+// core
 var gulp = require('gulp');
-var exec = require('child_process').exec;
-// var spawn = require('child_process').spawn;
+var gutil = require('gulp-util');
+// var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+// var debug = require('gulp-debug');
+
+// css
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+
+// js
 var cssnano = require('cssnano');
 var uglify = require('gulp-uglify');
 
+// tasks
 gulp.task('css', function () {
   var plugins = [
     autoprefixer(),
     cssnano()
   ];
   return gulp.src('_assets/sass/**/*.scss')
+    // .pipe(debug({title: 'Debug (css):'}))
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(plugins))
     .pipe(gulp.dest('_site/assets/css'));
@@ -20,51 +29,54 @@ gulp.task('css', function () {
 
 gulp.task('js', function () {
   return gulp.src('_assets/js/**/*.js')
+    // .pipe(debug({title: 'Debug (js):'}))
     .pipe(uglify())
     .pipe(gulp.dest('_site/assets/js'));
 });
 
-// EXEC NO WATCH
+// BTW: jekyll build process clobbers everything in _site, but excludes js and css dirs in _site/assets. this is specified in _config.yml (add to separate gulp-specific jekyll config?)
+// is it ok to skip adding callback to task?
+// removed --watch for now (letting Gulp handle this)
+// spawn vs exec: spawn has better log formatting via stdio, also performance possibly better
+// jekyll seems more lenient with errors when building with --incremental
+
+// EXEC (TASK WITH NO CALLBACK)
 // gulp.task('jekyll', function () {
 //   exec('bundle exec jekyll build --incremental');
 // });
-//
-// SPAWN NO WATCH
+
+// SPAWN (TASK WITH NO CALLBACK)
 // gulp.task('jekyll', function () {
 //   spawn('bundle', ['exec', 'jekyll', 'build', '--incremental']);
 // });
-//
-// EXEC WATCH
-// gulp.task('jekyll', function () {
-//   exec('bundle exec jekyll build --incremental --watch');
-// });
-//
-// SPAWN WATCH
-// gulp.task('jekyll', function () {
-//   spawn('bundle', ['exec', 'jekyll', 'build', '--incremental', '--watch']);
-// });
-//
-// EXEC NO WATCH WITH CB
-gulp.task('jekyll', function (cb) {
-  // jekyll build process clobbers everything in _site, but excludes js and css dirs in _site/assets. this is specified in _config.yml (add to separate gulp-specific jekyll config?)
-  exec('bundle exec jekyll build --incremental', function (err) {
-    if (err) return cb(err); // return error
-    cb(); // finished task
-  });
+
+// EXEC WITH LOGGING VIA CALLBACK (TASK WITH NO CALLBACK)
+// ...
+
+// SPAWN WITH LOGGING VIA INHERITED STDIO (TASK WITH NO CALLBACK)
+gulp.task('jekyll', function () {
+  spawn('bundle', ['exec', 'jekyll', 'build', '--incremental'], {stdio: 'inherit'})
 });
-//
-// SPAWN WATCH WITH LOGGING
-// gulp.task('jekyll', function () {
-//   var child = spawn('bundle', ['exec', 'jekyll', 'build', '--incremental', '--watch'])
-//
-//   child.stdout.on('data', (data) => {
-//     console.log(`stdout: ${data}`);
-//   });
-//
-//   child.stderr.on('data', (data) => {
-//     console.log(`stderr: ${data}`);
+
+// EXEC WITH LOGGING VIA CALLBACK (TASK WITH CALLBACK)
+// gulp.task('jekyll', function (done) {
+//   exec('bundle exec jekyll build --incremental', function (error, stdout, stderr) {
+//     if (error) {
+//         gutil.log('Jekyll: ', error);
+//         return;
+//     }
+//     gutil.log('Jekyll: ', stdout);
+//     gutil.log('Jekyll: ', stderr);
+//     done();
 //   });
 // });
+
+// SPAWN WITH LOGGING VIA INHERITED STDIO (TASK WITH CALLBACK)
+// gulp.task('jekyll', function (done) {
+//   spawn('bundle', ['exec', 'jekyll', 'build', '--incremental'], {stdio: 'inherit'})
+//     .on('close', done);
+// });
+
 
 gulp.task('watcher', function () {
   gulp.watch('_assets/sass/**/*.scss', ['css']);
