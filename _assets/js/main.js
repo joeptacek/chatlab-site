a653c3271e3f49548d13a3b05e719ce424f0746f
Joe Ptacek <joe.ptacek@gmail.com>
2018-02-15 13:22:33 -0500

    working on js for toggle-panels

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -87,83 +87,165 @@ window.addEventListener("scroll", function () {
 //   });
 // }
 
-// TODO: clean up old css for nav-site / nav-page - separate out any styles related to js (e.g., hiding buttons at larger breakpoints - otherwise regular non-mobile menus won't work)
-// if there are any toggle buttons
-if (toggleButtons = document.getElementsByName("toggle-button")) {
-  var thisToggleButton, buttonType, toggleTargets, sib, opening;
-
-  // TODO: create SVG element with js, based on button value
-  // TODO: ...or even better, create the whole button for every toggle-panel, based on additional class name 
-
+// if there are any toggle panels
+if (togglePanels = document.getElementsByClassName("toggle-panel")) {
   // event handler for toggle buttons
-  function toggleState() {
-    thisToggleButton = this; // for functions used as event handlers `this` refers to the element the event was fired from (i.e., equivalent to passing `event` to function and using event.currentTarget)
+  // var thisToggleButton, buttonType, toggleTargets, sib, opening;
+  // function toggleState() {
+  //   thisToggleButton = this; // for functions used as event handlers `this` refers to the element the event was fired from (i.e., equivalent to passing `event` to function and using event.currentTarget)
+  //
+  //   // setup: gather local targets (siblings with class .toggle-target)
+  //   // TODO: just grab first local target?
+  //   toggleTargets = [];
+  //   sib = thisToggleButton.parentNode.firstChild;
+  //   do {
+  //     // search button's siblings for every local toggle-target
+  //     if (sib.nodeType == 1 && sib.classList.contains("toggle-target")) {
+  //       // if sibling is an element type node (skip text nodes)
+  //       toggleTargets.push(sib);
+  //     }
+  //   } while (sib = sib.nextSibling);
+  //
+  //   // do the toggle based on toggle type (button value) and current toggle state
+  //   buttonType = thisToggleButton.value;
+  //   for (var i = 0; i < toggleTargets.length; i++) {
+  //     this.blur(); // get rid of css button outline the accesible way
+  //
+  //     // check current toggle state (for icon switching, below) and then toggle the state
+  //     switch (buttonType) {
+  //       case "menu":
+  //       case "drawer":
+  //         opening = toggleTargets[i].classList.contains("no-display");
+  //         toggleTargets[i].classList.toggle("no-display");
+  //         break;
+  //       case "menu-only-mobile":
+  //       case "drawer-only-mobile":
+  //         opening = toggleTargets[i].classList.contains("no-display-only-mobile");
+  //         toggleTargets[i].classList.toggle("no-display-only-mobile");
+  //         break;
+  //       case "drawer-incremental":
+  //         break;
+  //       case "drawer-incremental-only-mobile":
+  //         break;
+  //     }
+  //
+  //     // switch icon used based on toggle type and current toggle state
+  //     switch (buttonType) {
+  //       case "menu":
+  //       case "menu-only-mobile":
+  //         if (opening) {
+  //           thisToggleButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-close");
+  //         } else {
+  //           // closing
+  //           thisToggleButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-menu");
+  //         }
+  //         break;
+  //       case "drawer":
+  //       case "drawer-only-mobile":
+  //       case "drawer-incremental":
+  //       case "drawer-incremental-only-mobile":
+  //         if (opening) {
+  //           thisToggleButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-chevron-up");
+  //         } else {
+  //           // closing
+  //           thisToggleButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-chevron-down");
+  //         }
+  //         break;
+  //     }
+  //   }
+  // }
 
-    // setup: gather local targets (siblings with class .toggle-target)
-    // TODO: just grab first local target?
-    toggleTargets = [];
-    sib = thisToggleButton.parentNode.firstChild;
-    do {
-      // search button's siblings for every local toggle-target
-      if (sib.nodeType == 1 && sib.classList.contains("toggle-target")) {
-        // if sibling is an element type node (skip text nodes)
-        toggleTargets.push(sib);
-      }
-    } while (sib = sib.nextSibling);
+  // event handlers for different tp-types
+  var opening, clickedButton, localToggleTarget;
+  function toggleMenu() {
+    clickedButton = this;
+    clickedButton.blur(); // get rid of css button outline the accesible way
+    localToggleTarget = clickedButton.parentNode.getElementsByClassName("toggle-target")[0]; // if multiple toggle-targets, will only grab the first one
 
-    // do the toggle based on toggle type (button value) and current toggle state
-    buttonType = thisToggleButton.value;
-    for (var i = 0; i < toggleTargets.length; i++) {
-      this.blur(); // get rid of css button outline the accesible way
-
-      // check current toggle state (for icon switching, below) and then toggle the state
-      switch (buttonType) {
-        case "menu":
-        case "drawer":
-          opening = toggleTargets[i].classList.contains("no-display");
-          toggleTargets[i].classList.toggle("no-display");
-          break;
-        case "menu-only-mobile":
-        case "drawer-only-mobile":
-          opening = toggleTargets[i].classList.contains("no-display-only-mobile");
-          toggleTargets[i].classList.toggle("no-display-only-mobile");
-          break;
-        case "drawer-incremental":
-          break;
-        case "drawer-incremental-only-mobile":
-          break;
-      }
-
-      // switch icon used based on toggle type and current toggle state
-      switch (buttonType) {
-        case "menu":
-        case "menu-only-mobile":
-          if (opening) {
-            thisToggleButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-close");
-          } else {
-            // closing
-            thisToggleButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-menu");
-          }
-          break;
-        case "drawer":
-        case "drawer-only-mobile":
-        case "drawer-incremental":
-        case "drawer-incremental-only-mobile":
-          if (opening) {
-            thisToggleButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-chevron-up");
-          } else {
-            // closing
-            thisToggleButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-chevron-down");
-          }
-          break;
-      }
+    opening = localToggleTarget.classList.contains("tp-no-display");
+    localToggleTarget.classList.toggle("tp-no-display");
+    if (opening) {
+      clickedButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-close");
+    } else {
+      // closing
+      clickedButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-menu");
     }
   }
 
-  for (var i = 0; i < toggleButtons.length; i++) {
-    toggleButtons[i].addEventListener("click", toggleState);
-    toggleButtons[i].click(); // initialize by clicking all closable buttons (i.e., not incremental)
-    // for incremental button, don't initialize via click - need to explicitly set display state
+  function toggleDrawer() {
+    clickedButton = this;
+    clickedButton.blur(); // get rid of css button outline the accesible way
+    localToggleTarget = clickedButton.parentNode.getElementsByClassName("toggle-target")[0]; // if multiple toggle-targets, will only grab the first one
+
+    opening = localToggleTarget.classList.contains("tp-no-display");
+    localToggleTarget.classList.toggle("tp-no-display");
+    if (opening) {
+      clickedButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-chevron-up");
+    } else {
+      // closing
+      clickedButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-chevron-down");
+    }
+  }
+
+  function toggleDrawerIncremental() {
+    clickedButton = this;
+    clickedButton.blur(); // get rid of css button outline the accesible way
+    localToggleTarget = clickedButton.parentNode.getElementsByClassName("toggle-target")[0]; // if multiple toggle-targets, will only grab the first one
+
+    opening = localToggleTarget.classList.contains("tp-no-display");
+    localToggleTarget.classList.toggle("tp-no-display");
+    if (opening) {
+      clickedButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-chevron-up");
+    } else {
+      // closing
+      clickedButton.getElementsByTagName("use")[0].setAttribute("xlink:href", "#icon-chevron-down");
+    }
+  }
+
+  // add buttons (button > svg > use) to toggle-panels based on tp-type
+  var newButton, newSVG, newUse, thisTogglePanel, thisToggleTarget;
+  var svgns = "http://www.w3.org/2000/svg";
+  var xlinkns = "http://www.w3.org/1999/xlink";
+  for (var i = 0; i < togglePanels.length; i++) {
+    thisTogglePanel = togglePanels[i];
+    thisToggleTarget = thisTogglePanel.getElementsByClassName("toggle-target")[0]; // if multiple toggle-targets, will only grab the first one
+
+    // only add buttons etc. if toggle-panel has a toggle-target (not the case for page nav when sections <= 1)
+    if (thisToggleTarget) {
+      newButton = document.createElement("button");
+      newButton.setAttribute("type", "button");
+
+      newSVG = document.createElementNS(svgns, "svg"); // need to use createElementNS for SVG elements
+      newButton.appendChild(newSVG);
+
+      newUse = document.createElementNS(svgns, "use");
+
+      // add event handler / initialize tp state according to tp-type
+      if (thisTogglePanel.classList.contains("tp-type-menu")) {
+        newButton.addEventListener("click", toggleMenu);
+
+        // initialize tp state
+        thisToggleTarget.classList.toggle("tp-no-display");
+        newUse.setAttributeNS(xlinkns, "xlink:href", "#icon-menu"); // need to use setAttributeNS for xlink attributes
+
+      } else if (thisTogglePanel.classList.contains("tp-type-drawer")) {
+        newButton.addEventListener("click", toggleDrawer);
+
+        // initialize tp state
+        thisToggleTarget.classList.toggle("tp-no-display");
+        newUse.setAttributeNS(xlinkns, "xlink:href", "#icon-chevron-down");
+
+      } else if (thisTogglePanel.classList.contains("tp-type-drawer-incremental")) {
+        newButton.addEventListener("click", toggleDrawerIncremental);
+
+        // initialize tp state (add new function for drawer-incremental)
+        thisToggleTarget.classList.toggle("tp-no-display");
+        newUse.setAttributeNS(xlinkns, "xlink:href", "#icon-chevron-down");
+      }
+
+      newSVG.appendChild(newUse);
+      thisTogglePanel.appendChild(newButton);
+    }
   }
 }
 
